55dd4a0476ce53cb6bac7e4a393c66cb8566680b
Ankit Pratap <pratapguptaankit@gmail.com>
2025-03-18 13:37:54 +0530

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,7 +12,7 @@ import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
 
-// Prevent the splash screen from auto-hiding before asset loading is complejhjhte.hvhgh
+// Prevent the splash screen from auto-hiding before asset loading is completed
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
